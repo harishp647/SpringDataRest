e0ac180103096fb13679c8837c92659a7f2bb279
user <user@gmail.com>
2019-01-15 13:14:52 -0800

    First commit
    Uploading all the files

---
+++ SpringDataRest/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into alien values(101,'Navin','Java');
+insert into alien values(102,'pravin','Android');
+insert into alien values(103,'Akshay','DataScience');
+insert into alien values(104,'Sindhu','Git');
+insert into alien values(105,'Prakash','Maths');
