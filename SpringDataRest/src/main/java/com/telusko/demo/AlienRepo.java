4ce391beec3a15d3fdd96d6c6fae5f0d477a28e4
harishp647 <harishp1647@gmail.com>
2021-07-05 19:51:10 -0500

    updating JPA repository again

--- SpringDataRest/src/main/java/com/telusko/demo/AlienRepo.java
+++ SpringDataRest/src/main/java/com/telusko/demo/AlienRepo.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.telusko.demo.model.Alien;
-
 // This is a JPA repository interface which enables access to CRUD methods
 @RepositoryRestResource(collectionResourceRel="aliens",path="aliens")
 public interface AlienRepo extends JpaRepository<Alien,Integer>
