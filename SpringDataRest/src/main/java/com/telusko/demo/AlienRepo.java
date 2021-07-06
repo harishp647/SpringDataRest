95920563a03382a81762d4d7362e721b67ec5d06
harishp647 <harishp1647@gmail.com>
2021-07-05 19:41:09 -0500

    updating JPA repository interface file

--- SpringDataRest/src/main/java/com/telusko/demo/AlienRepo.java
+++ SpringDataRest/src/main/java/com/telusko/demo/AlienRepo.java
@@ -5,10 +5,11 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.telusko.demo.model.Alien;
 
+// This is a JPA repository interface which enables access to CRUD methods
 @RepositoryRestResource(collectionResourceRel="aliens",path="aliens")
 public interface AlienRepo extends JpaRepository<Alien,Integer>
 {
 
-	
+
 	
 }
